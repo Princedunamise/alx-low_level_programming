cf6035b0b0f46c4865a001c7bc8ed9da41485585
princedunamise <obasichiemerie11@gmail.com>
2022-10-17 08:53:38 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry
+* Return: Always 0
+*/
+/* prints numbers of base 16 in lowercase */
+int main(void)
+{
+int num;
+char la;
+
+for (num = '0'; num < 16; num++)
+putchar((num % 16) + '0');
+
+for (la = 'a'; la <= 'f'; la++)
+putchar(la);
+
+putchar('\n');
+return (0);
+}
