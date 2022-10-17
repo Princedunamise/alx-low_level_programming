166fab1d220303679a06e21c3f53fdfdb9ef8fcb
princedunamise <obasichiemerie11@gmail.com>
2022-10-17 06:32:14 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry
+* Return: Always 0
+*/
+/* prints alpha in lower case */
+int main(void)
+{
+char lc;
+for (lc = 'a'; lc <= 'Z', lc++;);
+{
+putchar(lc);
+}
+putchar('\n');
+return (0);
+}
