983ec1dd86068f17ca3dac51c9156e51e52c2084
princedunamise <obasichiemerie11@gmail.com>
2022-10-17 07:32:22 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry
+* Return: Always 0
+*/
+/* prints all string from 0 to 9 */
+int main(void)
+{
+int num;
+
+for  (num = 0; num <= 10; num++)
+printf("%d", num);
+printf("\n");
+return (0);
+}
+
