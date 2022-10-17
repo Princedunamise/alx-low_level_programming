0bc3b5e582fd85b5baf9e3e9e36bb8196c420e41
princedunamise <obasichiemerie11@gmail.com>
2022-10-17 10:01:42 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry
+* Return: Always 0
+*/
+/* prints combination of two digits */
+int main(void)
+{
+int x;
+
+for (x = 0; x <= 10; x++)
+{
+putchar((x / 10) + '0');
+putchar((x % 10) + '0');
+putchar('\n');
+}
+return (0);
+}
+
