8246d850b22e40ad1ae831c4f4c0e9eccee67337
princedunamise <obasichiemerie11@gmail.com>
2022-10-17 06:25:06 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ printf("Last digit of %d is %d and is greaer than 5\n", n, a);
 }
 else if (n == 0)
 {
-printf("Last digit of %d is zero and is 0\n", n, a);
+printf("Last digit of %d is %d and is 0\n", n, a);
 }
 else
 {
