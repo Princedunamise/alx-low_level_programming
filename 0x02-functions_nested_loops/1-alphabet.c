fb34cdf798ab3132a6ed6e6c0f843c29689224bd
princedunamise <obasichiemerie11@gmail.com>
2022-10-19 08:49:09 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+* print_alphabet - print alpha in lowercase followed by a newline
+*
+* Return: Always 0
+*/
+void print_alphabet(void)
+{
+char la;
+for (la = 'a'; la <= 'z'; la++)
+_putchar(la);
+
+_putchar('\n');
+}
